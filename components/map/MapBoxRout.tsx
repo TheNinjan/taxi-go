7c160eb2778e8ba2f05ddd1f7b4827ef26b45a9d
N100lanki <theninjan.com@gmail.com>
2023-09-01 15:30:19 +0530

    Update:MapBoxRoute

--- components/map/MapBoxRout.tsx
+++ components/map/MapBoxRout.tsx
@@ -3,10 +3,11 @@ import Map, { AttributionControl, GeolocateControl , Layer, Marker, NavigationCo
     Popup, Source } from "react-map-gl";
 
 function MapBoxRout(props:any) {
+  
   return (
-    <Source type="geojson" 
-    data={{ type: 'Feature', geometry: 
-{ type: 'LineString', coordinates: props.coordinates } }}>
+    <Source type="geojson" data={
+      {"type" : "FeatureCollection", "features" : [{"type": "Feature", "geometry": {"type":"Point","coordinates":[1,1]}, "properties": {"id": 1, "name": "one"}}, {"type": "Feature", "geometry": {"type":"Point","coordinates":[2,2]}, "properties": {"id": 2, "name": "two"}}, {"type": "Feature", "geometry": {"type":"Point","coordinates":[3,3]}, "properties": {"id": 3, "name": "three"}}]}
+}>
         <Layer
           type="line"
           layout={{ 'line-join': 'round', 'line-cap': 'square' }}
